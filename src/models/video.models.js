fd0612873c949a1cfa07c9479be9f43fbe532794
Rushikesh Chandanshiv <chandanshiv406@gmail.com>
2025-06-21 20:32:01 +0530

    video model complete

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,12 +1,47 @@
 import mongoose from "mongoose";
-import User from "./user.models";
+
 
 const videoSchema = new mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
-    user_id:[{
-        
-    }]
-
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    videoUrl:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    thumbnailUrl:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    category:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    tags:[
+        {
+            type:String,
+            trim:true
+        }
+    ],
+    likedBy:[{type: mongoose.Schema.Types.ObjectId ,ref:"User"}],
+    dislikedBy:[{type: mongoose.Schema.Types.ObjectId ,ref:"User"}],
+    viewedBy:[{type: mongoose.Schema.Types.ObjectId ,ref:"User"}],
 },{timestamps:true})
 
 const Video = mongoose.model("Video",videoSchema)
